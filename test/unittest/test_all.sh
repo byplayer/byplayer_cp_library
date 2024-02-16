8e345ce13555bc021c80e2241036f01a8c1b1a2a
byplayer <byplayer100@gmail.com>
2024-02-16 14:32:59 +0900

    🐛 fix github action test error
    replace_obj_path is stored in bin directory so I use relative path
    using it in makefile.
    
    And I add pre-commit hook to test all.

---
+++ test/unittest/test_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+function make_test() {
+    CXX=$1
+    CXX_VER=$2
+    echo test ${CXX} with ${CXX_VER}
+    make CXX=${CXX} CXX_VER=${CXX_VER} clean
+    make CXX=${CXX} CXX_VER=${CXX_VER} test
+    make CXX=${CXX} CXX_VER=${CXX_VER} clean
+}
+
+make_test clang++ c++23
+make_test clang++ c++20
+make_test clang++ c++17
+make_test g++ c++23
+make_test g++ c++20
+make_test g++ c++17
