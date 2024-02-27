acd73e9fd6071fe4ce86c1d4396ae59cb03aea93
byplayer <byplayer100@gmail.com>
2024-02-27 17:19:06 +0900

    👕 suppress some warning which are in gtest

--- test/unittest/include/gtest_include.hpp
+++ test/unittest/include/gtest_include.hpp
@@ -2,6 +2,8 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wpadded"
 #pragma clang diagnostic ignored "-Wweak-vtables"
+#pragma clang diagnostic ignored "-Wunsafe-buffer-usage"
+#pragma clang diagnostic ignored "-Wreserved-identifier"
 #endif // __clang__
 #include <gtest/gtest.h>
 #ifdef __clang__
