c36020cdfeb7044614949a840aebd54ed8cb74e6
byplayer <byplayer100@gmail.com>
2024-03-05 15:17:50 +0900

    👍 update change log
    update change log 0.2.0

--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -1,10 +1,12 @@
 var searchData=
 [
-  ['2_202024_2002_2015_0',['0.0.2 / 2024-02-15',['../d3/de2/md_changelog.html#autotoc_md24',1,'']]],
-  ['2024_2002_2014_1',['0.0.1 / 2024-02-14',['../d3/de2/md_changelog.html#autotoc_md25',1,'']]],
-  ['2024_2002_2015_2',['2024 02 15',['../d3/de2/md_changelog.html#autotoc_md23',1,'0.0.３ / 2024-02-15'],['../d3/de2/md_changelog.html#autotoc_md24',1,'0.0.2 / 2024-02-15']]],
-  ['2024_2002_2027_3',['0.1.0 / 2024-02-27',['../d3/de2/md_changelog.html#autotoc_md22',1,'']]],
-  ['2024_20ndash_2002_2028_4',['0.1.1 / 2024&amp;ndash;02-28',['../d3/de2/md_changelog.html#autotoc_md21',1,'']]],
-  ['27_5',['0.1.0 / 2024-02-27',['../d3/de2/md_changelog.html#autotoc_md22',1,'']]],
-  ['28_6',['0.1.1 / 2024&amp;ndash;02-28',['../d3/de2/md_changelog.html#autotoc_md21',1,'']]]
+  ['2_200_202024_2003_2005_0',['0.2.0 / 2024-03-05',['../d3/de2/md_changelog.html#autotoc_md21',1,'']]],
+  ['2_202024_2002_2015_1',['0.0.2 / 2024-02-15',['../d3/de2/md_changelog.html#autotoc_md25',1,'']]],
+  ['2024_2002_2014_2',['0.0.1 / 2024-02-14',['../d3/de2/md_changelog.html#autotoc_md26',1,'']]],
+  ['2024_2002_2015_3',['2024 02 15',['../d3/de2/md_changelog.html#autotoc_md24',1,'0.0.３ / 2024-02-15'],['../d3/de2/md_changelog.html#autotoc_md25',1,'0.0.2 / 2024-02-15']]],
+  ['2024_2002_2027_4',['0.1.0 / 2024-02-27',['../d3/de2/md_changelog.html#autotoc_md23',1,'']]],
+  ['2024_2002_2028_5',['0.1.1 / 2024-02-28',['../d3/de2/md_changelog.html#autotoc_md22',1,'']]],
+  ['2024_2003_2005_6',['0.2.0 / 2024-03-05',['../d3/de2/md_changelog.html#autotoc_md21',1,'']]],
+  ['27_7',['0.1.0 / 2024-02-27',['../d3/de2/md_changelog.html#autotoc_md23',1,'']]],
+  ['28_8',['0.1.1 / 2024-02-28',['../d3/de2/md_changelog.html#autotoc_md22',1,'']]]
 ];
