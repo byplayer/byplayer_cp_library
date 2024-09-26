a06f4f78d36b1d00fefd02d50ea06676abf478ff
byplayer <byplayer100@gmail.com>
2024-09-26 12:26:55 +0900

    📝 Update change log

--- docs/search/all_13.js
+++ docs/search/all_13.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['s_20competitive_20programming_20library_0',['byplayer&apos;s competitive programming library',['../index.html',1,'']]],
-  ['shortest_20path_20最短経路探索_1',['Finding shortest path(最短経路探索)',['../df/dc4/md_libraries.html#autotoc_md9',1,'']]],
-  ['structbcpl_5f1_5f1dijkstra_20html_20dijkstra_20method_20ダイクストラ法_20a_2',['&lt;a href=&quot;dc/d3b/structbcpl_1_1dijkstra.html&quot; &gt;Dijkstra method(ダイクストラ法)&lt;/a&gt;',['../df/dc4/md_libraries.html#autotoc_md10',1,'']]],
-  ['submitting_3',['expand libraries into your code before submitting',['../index.html#autotoc_md3',1,'']]]
+  ['readme_2emd_0',['readme.md',['../d0/d20/readme_8md.html',1,'']]],
+  ['reference_1',['reference',['../index.html#autotoc_md4',1,'']]]
 ];
--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -1,13 +1,15 @@
 var searchData=
 [
-  ['2_200_202024_2003_2005_0',['0.2.0 / 2024-03-05',['../d3/de2/md_changelog.html#autotoc_md24',1,'']]],
-  ['2_202024_2002_2015_1',['0.0.2 / 2024-02-15',['../d3/de2/md_changelog.html#autotoc_md28',1,'']]],
-  ['2024_2002_2014_2',['0.0.1 / 2024-02-14',['../d3/de2/md_changelog.html#autotoc_md29',1,'']]],
-  ['2024_2002_2015_3',['2024 02 15',['../d3/de2/md_changelog.html#autotoc_md27',1,'0.0.３ / 2024-02-15'],['../d3/de2/md_changelog.html#autotoc_md28',1,'0.0.2 / 2024-02-15']]],
-  ['2024_2002_2027_4',['0.1.0 / 2024-02-27',['../d3/de2/md_changelog.html#autotoc_md26',1,'']]],
-  ['2024_2002_2028_5',['0.1.1 / 2024-02-28',['../d3/de2/md_changelog.html#autotoc_md25',1,'']]],
-  ['2024_2003_2005_6',['0.2.0 / 2024-03-05',['../d3/de2/md_changelog.html#autotoc_md24',1,'']]],
-  ['2024_2008_2017_7',['0.3.0 / 2024-08-17',['../d3/de2/md_changelog.html#autotoc_md23',1,'']]],
-  ['27_8',['0.1.0 / 2024-02-27',['../d3/de2/md_changelog.html#autotoc_md26',1,'']]],
-  ['28_9',['0.1.1 / 2024-02-28',['../d3/de2/md_changelog.html#autotoc_md25',1,'']]]
+  ['2_200_202024_2003_2005_0',['0.2.0 / 2024-03-05',['../d3/de2/md_changelog.html#autotoc_md25',1,'']]],
+  ['2_202024_2002_2015_1',['0.0.2 / 2024-02-15',['../d3/de2/md_changelog.html#autotoc_md29',1,'']]],
+  ['2024_2002_2014_2',['0.0.1 / 2024-02-14',['../d3/de2/md_changelog.html#autotoc_md30',1,'']]],
+  ['2024_2002_2015_3',['2024 02 15',['../d3/de2/md_changelog.html#autotoc_md28',1,'0.0.３ / 2024-02-15'],['../d3/de2/md_changelog.html#autotoc_md29',1,'0.0.2 / 2024-02-15']]],
+  ['2024_2002_2027_4',['0.1.0 / 2024-02-27',['../d3/de2/md_changelog.html#autotoc_md27',1,'']]],
+  ['2024_2002_2028_5',['0.1.1 / 2024-02-28',['../d3/de2/md_changelog.html#autotoc_md26',1,'']]],
+  ['2024_2003_2005_6',['0.2.0 / 2024-03-05',['../d3/de2/md_changelog.html#autotoc_md25',1,'']]],
+  ['2024_2008_2017_7',['0.3.0 / 2024-08-17',['../d3/de2/md_changelog.html#autotoc_md24',1,'']]],
+  ['2024_2009_2026_8',['0.4.0 / 2024-09-26',['../d3/de2/md_changelog.html#autotoc_md23',1,'']]],
+  ['26_9',['0.4.0 / 2024-09-26',['../d3/de2/md_changelog.html#autotoc_md23',1,'']]],
+  ['27_10',['0.1.0 / 2024-02-27',['../d3/de2/md_changelog.html#autotoc_md27',1,'']]],
+  ['28_11',['0.1.1 / 2024-02-28',['../d3/de2/md_changelog.html#autotoc_md26',1,'']]]
 ];
